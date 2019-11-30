78fe5db5b750dda68bd88bfabb59c8e6ec4dbbb6
Gerolf Ziegenhain <g@ziegenhain.com>
2019-11-30 01:17:51 +0100

    wip

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@ set -e
 echo "Create config file"
 cat << eof > /etc/ser2net.conf
 $TTY_CAT_PORT:telnet:600:$TTY_CAT:$TTY_CAT_CONF
+$TTY_CAT_PORT2:telnet:600:$TTY_CAT:$TTY_CAT_CONF
 $TTY_PTT_PORT:telnet:600:$TTY_PTT:$TTY_PTT_CONF
 eof
-#$TTY_CAT_PORT2:telnet:600:$TTY_CAT:$TTY_CAT_CONF
 
 echo "Start source CAT port"
 ser2net -d &
