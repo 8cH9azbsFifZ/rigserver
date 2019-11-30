f8c97d03de4c34e609ef91da33f9bec457970f77
Gerolf Ziegenhain <g@ziegenhain.com>
2019-11-30 23:15:28 +0100

    wip

--- startup.sh
+++ startup.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "Create config file"
 cat << eof > /etc/ser2net.conf
-$TTY_CAT_PORT:telnet:600:$TTY_CAT:$TTY_CAT_CONF
+$TTY_CAT_PORT:telnet:600:$TTY_CAT:$TTY_CAT_CONF max-connections=10
 $TTY_CAT_PORT2:telnet:600:$TTY_CAT:$TTY_CAT_CONF
 $TTY_PTT_PORT:telnet:600:$TTY_PTT:$TTY_PTT_CONF
 eof
 #FIXME: PTT Port as RTS raw?
 
 #echo "Start source CAT port"
-#ser2net -d &
+ser2net -d &
 
 
 echo "Configure Pulseaudio"
